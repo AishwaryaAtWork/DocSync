75648c9bde8ebe0db786343208e1b73d920eefe6
Aishwarya Pathak <pathakaishwarya54@gmail.com>
2023-08-05 20:00:44 +0530

    Parallel connection established

--- server/server.js
+++ server/server.js
@@ -1,3 +1,8 @@
+const mongoose = require('mongoose');
+const Document = require('./Document')
+
+mongoose.connect('mongodb://127.0.0.1:27017/DocSync',{ useNewUrlParser: true, useUnifiedTopology: true});
+
 const io = require('socket.io')(3001, {
     cors: {
         origin: 'http://localhost:3000',
@@ -5,6 +10,28 @@ const io = require('socket.io')(3001, {
     }
 });
 
+const defaultValue = '';
+
 io.on('connection', socket =>{
-    console.log('Connected');
+    socket.on('get-document', async (documentId) => {
+        const document = await findOrCreateDocument(documentId);
+
+        socket.join(documentId);
+        socket.emit('load-document', document.data);
+        
+        socket.on('send-changes', (delta) => {
+            socket.broadcast.to(documentId).emit('recieve-changes', delta)
+        });
+        socket.on('save-document', async (data)=>{
+            await Document.findByIdAndUpdate(documentId, {data})
+        });
+    });
 });
+
+async function findOrCreateDocument(id) {
+    if (id === null) return
+  
+    const document = await Document.findById(id)
+    if (document) return document
+    return await Document.create({ _id: id, data: defaultValue })
+  }
